23276f55a909eefffad4b95fab37fd3d3d92e4a9
Gustavo Rodrigues <41215700+Ghustavo516@users.noreply.github.com>
2023-08-02 08:49:23 -0300

    Base Projeto

---
+++ src/test/java/br/com/api/API/Supermercado/ApiSupermercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.API.Supermercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSupermercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
